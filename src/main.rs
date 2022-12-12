98c9036a0ba4ffe3796bca8559e6d36d6e9ece86
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-12-12 15:49:07 +0900

    ㅁㅇㅇ

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use rand::Rng;
 
 fn n_power(mut x: i32, mut e: i32, m: i32) -> i32 {
     let mut res: i32 = 1;
-
+    //e가 0보다 클떄까지
     while e > 0 {
+        //e가 2로 나누어 떨어지면
         if (e % 2) == 1 {
             res = (res * x) % m;
             e = e - 1;
+        //아니면
         } else {
             x = (x * x) % m;
             e = e / 2;
@@ -17,26 +19,29 @@ fn n_power(mut x: i32, mut e: i32, m: i32) -> i32 {
 }
 
 fn is_bool(n: i32, k: i32) -> bool {
+    //n이 2로 나누어 떨어지거나 n이 2가 아니면 false
     if n % 2 == 0 && n != 2 {
         return false;
+        //n이 3보다 같거나 3보다 작으면 true
     } else if n <= 3 {
         return true;
     }
     while k > 0 {
         let mut rng = rand::thread_rng();
-        let test: i32 = rng.gen_range(0..1);
+        let test: i32 = rng.gen_range(1..10);
         let a: i32 = test * (n - 3) + 2;
         println!("{}", a);
-
+        //계산한 갓이 1이 아니면 false
         if n_power(a, n - 1, n) != 1 {
             return false;
         }
     }
-
+    //true리턴
     return true;
 }
 
 fn main() {
-    let num = rand::thread_rng().gen_range(0..100);
-    println!("{}", num);
+    is_bool(1, 2);
+    let num: f64 = rand::thread_rng().gen_range(0.0..1.0);
+    println!("{:?}", is_bool(1, 2));
 }
