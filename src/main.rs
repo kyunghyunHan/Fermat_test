be473ddfe54015f7d18ec3739c634b1c2d996c28
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-12-13 18:13:32 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,59 @@
-use rand::Rng;
-
-fn modulo(base: i32, mut exponent: i32, mode: i32) -> i32 {
-    let mut x = 1;
-    let mut y = base;
-    while exponent > 0 {
-        if exponent % 2 == 1 {
-            println!("{}", exponent);
-            x = (x * base) % mode;
-        }
-        y = (y * y) % mode;
-        exponent = exponent / 2;
-        println!("{}", exponent);
-    }
-    return x % mode;
+struct DivisorGen {
+    curr: u64,
+    last: u64,
 }
-fn fermat(p: i32, iterations: i32) -> bool {
-    if p == 1 {
-        return false;
+
+impl Iterator for DivisorGen {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        self.curr += 2u64;
+
+        if self.curr < self.last {
+            None
+        } else {
+            Some(self.curr)
+        }
     }
-    for i in 0..iterations {
-        let num = rand::thread_rng().gen_range(0..1);
-        let a = num % (p - 1) + 1;
-        if modulo(a, p - 1, p) != 1 {
-            return false;
+}
+
+fn divisor_gen(num: u64) -> DivisorGen {
+    DivisorGen {
+        curr: 0u64,
+        last: (num / 2u64) + 1u64,
+    }
+}
+
+fn is_prime(num: u64) -> bool {
+    if num == 2 || num == 3 {
+        return true;
+    } else if num % 2 == 0 || num % 3 == 0 || num <= 1 {
+        return false;
+    } else {
+        for i in divisor_gen(num) {
+            if num % i == 0 {
+                return false;
+            }
         }
     }
     return true;
 }
+
 fn main() {
-    if fermat(91, 100) {
-        println!("is prime")
-    } else {
-        println!("is not prime");
+    let fermat_closure = |i: u32| -> u64 { 2u64.pow(2u32.pow(i + 1u32)) };
+    let mut f_numbers: Vec<u64> = Vec::new();
+
+    println!("First 4 Fermat numbers:");
+    for i in 0..4 {
+        let f = fermat_closure(i) + 1u64;
+        f_numbers.push(f);
+        println!("F{}: {}", i, f);
+    }
+
+    println!("Factor of the first four numbers:");
+    for f in f_numbers.iter() {
+        let is_prime: bool = f % 4 == 1 && is_prime(*f);
+        let not_or_not = if is_prime { " " } else { " not " };
+        println!("{} is{}prime", f, not_or_not);
     }
 }
